941bc9a42952c460495e1b6913a145360a65e25c
flyzza <zfy1995@hotmail.com>
2015-01-27 22:07:28 +0100

    deleted unnecessary code

--- source/BoardAnimation.hx
+++ source/BoardAnimation.hx
@@ -35,24 +35,4 @@ class BoardAnimation extends Animation
 		
 		bitmapData = Assets.getBitmapData( "assets/rotationTilesheet.png" );
 	}
-	override function initializeTilesheet():Void
-	{
-		var cols:Int = Math.floor( bitmapData.width / imageWidth );
-		frameCount = Math.floor( frameCount );
-	
-		// create tileSheet object
-		tilesheet = new Tilesheet( bitmapData );
-
-		// loop to get all the seperate images from the sprite sheet
-		for( rotationAnimationIndex in 0...frameCount )
-		{
-			var col:Int = rotationAnimationIndex % cols;
-			var row:Int = Math.floor( rotationAnimationIndex / cols );
-			// calculate the rectangle of the next board's image
-			var rectangle:Rectangle = new Rectangle( imageWidth * col, imageHeight * row, imageWidth, imageHeight );
-
-			// add the rectangle of the next image in the sprite sheet
-			tilesheet.addTileRect( rectangle );
-		}
-	}
 }
