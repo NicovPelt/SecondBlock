925c8d4b26cb3382fbee3a79ec853a17245711d6
flyzza <zfy1995@hotmail.com>
2015-01-27 12:17:59 +0100

    Animations parentclass(broken)

--- source/BoardAnimation.hx
+++ source/BoardAnimation.hx
@@ -1,5 +1,8 @@
 package;
 
+import openfl.display.Tilesheet;
+import openfl.display.BitmapData;
+import openfl.Assets;
 import openfl.display.BitmapData;
 import openfl.display.Sprite;
 import openfl.display.Tilesheet;
@@ -7,90 +10,44 @@ import openfl.display.Tilesheet;
 import openfl.geom.Point;
 import openfl.geom.Rectangle;
 import openfl.Assets;
-
 /**
  * ...
  * @author Zhifan Yang
  */
-class BoardAnimation extends Sprite
+class BoardAnimation extends Animation
 {
-  // the total amount of images
-  var boardCount:Int = 11;
 
-  // the width and height of one image
-  var boardWidth:Int = 800;
-  var boardHeight:Int = 600;
+	public function new() 
+	{
+		super(frameCount, imageWidth, imageHeight, currentImage, bitmapData);
+		frameCount = 11;
+		// the width and height of one image
+		imageWidth = 800;
+		imageHeight = 600;
+		// the image that is currently visible
+		currentImage = 0;
+		
+		bitmapData = Assets.getBitmapData( "assets/rotationAnimation.png" );	
+	}
+	
+	override function initializeTilesheet():Void
+	{
+		var cols:Int = Math.floor( bitmapData.width / imageWidth );
+		frameCount = Math.floor( frameCount );
+	
+		// create tileSheet object
+		tilesheet = new Tilesheet( bitmapData );
 
-  // the image that is currently visible
-  var currentBoardImage:Int = 0;
+		// loop to get all the seperate images from the sprite sheet
+		for( rotationAnimationIndex in 0...frameCount )
+		{
+			var col:Int = rotationAnimationIndex % cols;
+			var row:Int = Math.floor( rotationAnimationIndex / cols );
+			// calculate the rectangle of the next board's image
+			var rectangle:Rectangle = new Rectangle( imageWidth * col, imageHeight * row, imageWidth, imageHeight );
 
-  // the OpenFL tilesheet object
-  var boardTilesheet:Tilesheet;
-
-  /**
-   * The constructor. 
-   * Initializes things. In this case the tilesheet with the animation
-   */
-  public function new()
-  {
-    super();
-
-    initializeTilesheet();
-  }
-
-  /**
-   * Update the board
-   * Called from 'outside' hence the public access-modifier
-   */
-  public function update():Void
-  {
-    drawNextImage();
-  }
-
-  /**
-   * Draw the next image up for the animation
-   *
-   */
-  function drawNextImage():Void
-  {
-    this.graphics.clear();
-
-    // draw the image with 'id' currentGooseImage onto this.graphics
-    // the array [ 0, 0, currentGoose ] indicates
-    // * the x position (in this case zero)
-    // * the y position (in this case zero)
-    // * the index of the image to draw (increments from 0 to 11 by use of the modulo operator: %)
-    boardTilesheet.drawTiles( this.graphics, [ 0, 0, currentBoardImage ], true );
-
-    // point to the next index of the tile sheet's images
-    currentBoardImage += 1;
-  }
-
-  /**
-   * Initialize the Tilesheet instance with the images of the sprite sheet
-   */
-  function initializeTilesheet():Void
-  {
-    // start at the top left of the image
-    var column:Int = 0;
-    var row:Int = 0;
-
-    // get image from assets folder and create tileSheet object
-    var boardBitmapData:BitmapData = Assets.getBitmapData( "assets/rotationTilesheet.png" );
-    boardTilesheet = new Tilesheet( boardBitmapData );
-
-    // loop to get all the seperate images from the sprite sheet
-    for( rotationTilesheetIndex in 0...boardCount )
-    {
-      // calculate the rectangle of the next board's image
-      var boardRectangle:Rectangle = new Rectangle( boardWidth * column, boardHeight * row, boardWidth, boardHeight );
-
-      // add the rectangle of the next image in the sprite sheet
-      boardTilesheet.addTileRect( boardRectangle );
-
-      // some math to recalculate the row and column for the next image of the animation
-      row = Math.floor( rotationTilesheetIndex / 4 );
-      column = rotationTilesheetIndex % 4;
-    }
-  }
+			// add the rectangle of the next image in the sprite sheet
+			tilesheet.addTileRect( rectangle );
+		}
+	}
 }
